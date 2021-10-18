d0b6163e2681354245f045086bed1279b7bfe61c
Khadija Abdulahi <khadijaabdulahi@gmail.com>
2021-10-18 16:13:20 -0500

    Completed functions.js

--- functions.js
+++ functions.js
@@ -1,13 +1,32 @@
 'use strict';
 
 // 1. isHometown
+function isHometown(town) {
+    return town === 'San Francisco';
+}
 
-// Define your function here
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`
+}
+
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax= 0.05){
+    const subtotal = basePrice * (1 + tax); 
+    let fee = 0;
+    if (state === 'CA') {
+        fee = 0.03 * subtotal;}
+    else if (state === 'PA'){
+        fee === 2;}
+    else if ( state === 'MA') {
+        if (basePrice <= 100);
+            fee = 1;} 
+        else { 
+            fee === 3;
+        }
+        return subtotal + fee 
+}
